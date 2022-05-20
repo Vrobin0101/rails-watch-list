7181cd8f2de72cfb9d509bf59fbe6e7e6d462305
Robin Varetz <vrobin@student.42.fr>
2022-05-20 17:34:40 +0200

    responsive

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_one_attached :photo
+  has_many :reviews, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
 
   validates_uniqueness_of :name
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, path: '/', only: %w[show new create] do
     resources :bookmarks, only: %w[new create]
+    resources :reviews, only: %w[new create]
   end
   resources :bookmarks, only: %w[destroy]
 end
